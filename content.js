de16cad7b05760876ca19eb8656f7543a4bd25d6
INSANE <70059960+Mr-SuperInsane@users.noreply.github.com>
2023-07-25 14:26:01 +0900

    ver2023/7/25

---
+++ content.js
@@ -0,0 +1,7 @@
+const YOUR_EMAIL = "gp23a000";
+const YOUR_PASSWORD = "your password";
+
+document.getElementById('username').value = YOUR_EMAIL;
+document.getElementById('password').value = YOUR_PASSWORD;
+
+document.getElementById('loginbtn').click();
